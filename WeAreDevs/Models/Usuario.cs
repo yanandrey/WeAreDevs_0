fe1b908f363d239d7c6c4b638b76022fbf3a99c2
Yan Andrey <43590571+yanandrey@users.noreply.github.com>
2023-06-06 10:52:07 -0300

    atualização do arquivo Usuario.cs

--- WeAreDevs/Models/Usuario.cs
+++ WeAreDevs/Models/Usuario.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
+        public string Email { get; set; }
         public int Idade { get; set; }
     }
 }
